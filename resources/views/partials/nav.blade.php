10e001326a7296fe23cc126695dd3e758dbfa5fe
Leib92 <allan.leibenau@tptlive.ee>
2021-10-25 10:40:10 +0300

    25.10.2021 pagination

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -9,6 +9,9 @@
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="admin/post" href="/">Home</a>
+                </li>
             </ul>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App/Http/Controllers/HomeController;
+use App/Http/Controllers/PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
-Route::get('/post/{post}', [\App\Http\Controllers\HomeController::class, 'post']);
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/post/{post}', [HomeController::class, 'post']);
+Route::get('/admin/posts', [PostController::class, 'index'])->name('admin.posts.index');
 
